5545bcb52dedb9d2beed534b112215fa7f69702c
Eyrenna <claudia365x@gmail.com>
2019-09-04 16:33:19 +0200

    Proyecto iniciado: pom, gitignore, app

---
+++ src/main/java/encinas/App.java
@@ -0,0 +1,30 @@
+package encinas;
+
+public final class App {
+    public static void main(String[] args) {
+
+        /**
+         * Configuració del programador de tasques
+         * del sistema amb el tipus de target triat:
+         * des de vehicles a qualsevol cosa que admiteix
+         * la recepció d'un missatge.
+         */
+        ProgramadorTasques programadorTasques = new ProgramadorTasques(new Vehicle());
+
+        /**
+         * Afegir al sistema les tasques que volem que el sistema
+         * executi al rebre la petició del client.
+         */
+        programadorTasques.setTasca(new Autenticacio());
+        programadorTasques.setTasca(new Autoritzacio());
+
+        /**
+         * Configuració de l'app client per a que
+         * executi les tasques programades i
+         * enviï el misstage al sistema.
+         */
+        Mollapp mollapp = new Mollapp();
+        mollapp.setProgramadorTasques(programadorTasques);
+        mollapp.enviarPeticio("Francesc");
+    }
+}
---
+++ src/test/java/encinas/AppTest.java
@@ -0,0 +1,20 @@
+package encinas;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
